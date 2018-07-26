b4bf99092443b854bc9b59d52e45d012a8274f37
Samsul Arif <karawangsaz@gmail.com>
2018-07-26 16:21:38 +0700

    Pembuatan halaman materials

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -15,6 +15,16 @@ class MaterialController extends Controller
         $materials = DB::table('materials')
             ->where('id_sector', $sector[0]->id)->get()->toArray();
 
+        for ($i=0; $i < count($materials); $i++) {
+            $some_series = DB::table('series')
+                ->select(DB::raw('nama, slug'))
+                ->where('id_material', $materials[$i]->id)
+                ->limit(3)
+                ->get()->toArray();
+
+            $materials[$i]->series = $some_series;
+        }
+
         $data = [
             'sector' => $sector[0],
             'materials' => $materials,
--- resources/views/pages/materials.blade.php
+++ resources/views/pages/materials.blade.php
@@ -7,12 +7,18 @@
         <div class="row">
             @foreach ($materials as $material)
                 <div class="col-md-4">
-                    <div class="card mt-4">
+                    <div class="card shadow-sm mt-5">
                         <img class="card-img-top" src="{{ $material->thumbnail }}" alt="{{ $material->nama }}">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $material->nama }}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="{{ url('material/' . $material->slug) }}" class="btn btn-primary">Selengkapnya</a>
+                            <h5 class="card-title text-center">{{ $material->nama }}</h5>
+                            <p class="card-text">
+                                <ul class="list-group list-group-flush">
+                                    @foreach ($material->series as $series)
+                                        <li class="list-group-item">{{ $series->nama }}</li>                                    
+                                    @endforeach
+                                </ul>
+                            </p>
+                            <a href="{{ url('material/' . $material->slug) }}" class="float-right btn btn-primary">Lebih banyak &raquo;</a>
                         </div>
                     </div>
                 </div>

90bc3e4b6a20491443fae88703746e66252dd317
Samsul Arif <karawangsaz@gmail.com>
2018-07-26 16:25:47 +0700

    Perbaikan style halaman materials

--- resources/views/pages/materials.blade.php
+++ resources/views/pages/materials.blade.php
@@ -12,11 +12,11 @@
                         <div class="card-body">
                             <h5 class="card-title text-center">{{ $material->nama }}</h5>
                             <p class="card-text">
-                                <ul class="list-group list-group-flush">
+                                <div class="list-group list-group-flush">
                                     @foreach ($material->series as $series)
-                                        <li class="list-group-item">{{ $series->nama }}</li>                                    
+                                        <a class="list-group-item list-group-item-action" href="{{ url('series/' . $series->slug) }}">{{ $series->nama }}</a>
                                     @endforeach
-                                </ul>
+                                </div>
                             </p>
                             <a href="{{ url('material/' . $material->slug) }}" class="float-right btn btn-primary">Lebih banyak &raquo;</a>
                         </div>
